218a393fda55976613f5d0b3b9057eecf34c8a94
pavk96 <rbwjd97@naver.com>
2020-08-26 14:46:15 +0900

    Updates

---
+++ precache-manifest.e2fa560bc138b876462054fe9dba6f45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28aab238d47fa70546a3a7bced3ae2bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69eb62f052c644f8ea3a",
+    "url": "/static/css/main.483dee2a.chunk.css"
+  },
+  {
+    "revision": "d79053e72a6d69d7dead",
+    "url": "/static/js/2.6f35e770.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f35e770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69eb62f052c644f8ea3a",
+    "url": "/static/js/main.32580070.chunk.js"
+  },
+  {
+    "revision": "04ae0d9c8b0eb04091af",
+    "url": "/static/js/runtime-main.b61f5236.js"
+  },
+  {
+    "revision": "539e2213eba73401d67784853e0b99b6",
+    "url": "/static/media/logo.539e2213.png"
+  }
+]);
